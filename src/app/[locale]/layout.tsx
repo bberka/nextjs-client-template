89a5d0799720f7c79446184ea17ca03d34989426
Berkay <berkaybayarg@gmail.com>
2026-02-06 23:44:36 +0300

    init

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,15 @@
+import { Providers } from "../providers";
+
+export default function LocaleLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
